374740b38ba0724b018ad45ad37e493dd5261fca
Courtney Noonan <courtneynoonan@me.com>
2017-07-05 13:44:39 -0400

    adding knockout

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
+const MAP_API_KEY = "AIzaSyBB9SLtMMAk06MZodCNZ7MuvJ2eX2ClGWM"
 
+var park = {
+  name: "Sibbald Point",
+  latLong: {lat: 44.3219564, lng: -79.3254315}
+}
+
+// map stuff
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var ontario_centre = {lat: 45.035609, lng: -79.085021};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+    zoom: 7,
+    center: ontario_centre
   });
+
   var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+    position: park.latLong,
+    map: map,
+    title: park.name
+  });
+
+  var contentString = "Hello"
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
