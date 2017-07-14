e275bccdae3b65adbdc59a01f0f3beececa2fa70
Courtney Noonan <courtneynoonan@me.com>
2017-07-14 18:47:35 -0400

    converting data to restaurants, and calling location data from facebook

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
 const MAP_API_KEY = "AIzaSyBB9SLtMMAk06MZodCNZ7MuvJ2eX2ClGWM"
+const APP_ID = '1564943346912728';
+const APP_SECRET = '1bcf3ad4d4708361f6988bce3ebb6d34';
 
-// initial park data
-var initialParks = [
+// initial restaurant data
+var locationData = [
   {
-    name: "Sibbald Point",
-    address: "26071 Park Rd, Jacksons Point",
-    lat: 44.3219564,
-    lng: -79.3254315,
-    infoContent: "content belongs here"
+    name: "Fishbone",
+    lat: 44.003317,
+    lng: -79.451036 ,
   }, {
-    name: "Awenda",
-    address: "670 Awenda Park Rd, Tiny",
-    lat: 44.846409,
-    lng: -79.998677,
-    infoContent: "content belongs here"
+    name: "L'il Brew Hops",
+    lat: 44.053113,
+    lng: -79.457247,
   }, {
-    name: "McRae Point",
-    address: "4366 McRae Park Road, Ramara Township",
-    lat: 44.5705296,
-    lng: -79.3243923,
-    infoContent: "content belongs here"
+    name: "Filly & Co",
+    lat: 43.991214,
+    lng: -79.465786,
   }
 ]
 
@@ -45,10 +41,8 @@ window.fbAsyncInit = function() {
 // location object constructor
 var Location = function(data) {
   this.name = ko.observable(data.name),
-  this.address = ko.observable(data.address),
   this.lat = ko.observable(data.lat),
-  this.lng = ko.observable(data.lng),
-  this.infoContent = ko.observable(data.infoContent)
+  this.lng = ko.observable(data.lng)
 }
 
 // array to store markers
@@ -56,26 +50,23 @@ var markers = [];
 
 // map initialization and display
 function initMap() {
-  var ontario_centre = {lat: 45.035609, lng: -79.085021};
+  var aurora_centre = {lat: 44.003335, lng: -79.450943 };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: ontario_centre
+    zoom: 11,
+    center: aurora_centre
   });
 
   // info window to display content when markers are clicked
   var infoWindow = new google.maps.InfoWindow();
 
-  // create markers for park locations
-  for (i=0; i < initialParks.length; i++) {
+  // create markers for restaurant locations
+  for (i=0; i < locationData.length; i++) {
     var marker = new google.maps.Marker({
-      position: {lat: initialParks[i].lat, lng: initialParks[i].lng},
+      position: {lat: locationData[i].lat, lng: locationData[i].lng},
       map: map,
-      title: initialParks[i].name
+      title: locationData[i].name
     });
     markers.push(marker);
-    // marker.addListener('mouseover', function() {
-    //   marker.setAnimation(google.maps.Animation.BOUNCE);
-    // });
     // populateInfoWindow on marker click
     marker.addListener('click', function(marker) {
       this.setAnimation(4);
@@ -86,14 +77,26 @@ function initMap() {
 
   // set infowindow content to marker data and open
 function populateInfoWindow(marker, infoWindow) {
-  var app_id = '1564943346912728';
-  var app_secret = '1bcf3ad4d4708361f6988bce3ebb6d34';
+  var latLng = marker.position.lat() + ',' + marker.position.lng();
   if (infoWindow.marker != marker) {
     infoWindow.setContent('');
     infoWindow.marker = marker;
-    infoWindow.setContent('<div>' + marker.title + '</div>');
-    FB.api('/113124472034820', {access_token: `${app_id}|${app_secret}`}, function(response) {
-    console.log(response);
+    FB.api('/search?type=place&center=' + latLng + '&q=' + marker.title + '&distance=1000&fields=checkins,about,description,website,location,overall_star_rating', {access_token: `${APP_ID}|${APP_SECRET}`}, function(response) {
+    // store possible locations in an array
+    var possibleLocations = response.data;
+    var fbData;
+    if (possibleLocations.length === 1) {
+      fbData = possibleLocations[0];
+    } else {
+      // sort list by checkins, and return the location with the most
+      possibleLocations.sort(function(a, b) {
+        var x = a.checkins;
+        var y = b.checkins;
+        return ((x < y) ? -1 : (( x > y) ? 1 : 0));
+      }).reverse();
+      fbData = possibleLocations[0];
+    }
+    infoWindow.setContent('<div><a href="' + fbData.website + '">' + marker.title + '</a></div><div>Rating:' + fbData.overall_star_rating + '</div>');
     });
     infoWindow.open(map, marker);
   }
@@ -103,25 +106,27 @@ var ViewModel = function() {
 
   var self = this
 
-  // observable array for parks
-  self.parkList = ko.observableArray([])
+  // observable array for restaurants
+  self.restaurantList = ko.observableArray([])
 
-  // observable array for filtered parks
+  // observable array for filtered restaurants
   self.filter = ko.observable('');
 
-  // create Location objects for each park and add to parkList array
-  initialParks.forEach(function(data) {
-    self.parkList.push(new Location(data));
+  // create Location objects for each restaurant and add to restaurantList array
+  locationData.forEach(function(data) {
+    self.restaurantList.push(new Location(data));
   });
 
   //
-  self.filteredParks = ko.computed(function() {
+  self.filteredRestaurants = ko.computed(function() {
     var filter = self.filter().toLowerCase();
+    // by default, return the restaurantList
     if (!filter) {
-        return self.parkList();
+        return self.restaurantList();
     } else {
-        return ko.utils.arrayFilter(self.parkList(), function(park) {
-            return park.name().toLowerCase().indexOf(filter) !== -1;
+        // filter restaurants for given letter sequence
+        return ko.utils.arrayFilter(self.restaurantList(), function(restaurant) {
+            return restaurant.name().toLowerCase().indexOf(filter) !== -1;
         });
     }
 }, ViewModel);
